06ac1f16755501345d65e2203a7838fc8360d602
Christian Limpach <Christian.Limpach@gmail.com>
2020-10-15 14:36:35 +0000

    add .dir-locals.el since codebase is tab-width 4 and hard tabs *ugh*

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (tab-width . 4)
+  (indent-tabs-mode . t))
+ (c-mode
+  (c-file-style . "BSD")
+  (c-basic-offset . 4)))
+
